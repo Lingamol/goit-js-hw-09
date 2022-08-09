8d8b0963ad20354b2dc7ad63f6d191d2f4e8437c
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-08-10 01:46:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+refs.btnStart.addEventListener('click', handleOnStartBtn);
+refs.btnStop.addEventListener('click', handleOnStop);
+function handleOnStartBtn() {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  refs.btnStart.setAttribute('disabled', true);
+}
+function handleOnStop() {
+  refs.btnStart.removeAttribute('disabled', true);
+  clearInterval(timerId);
+}
