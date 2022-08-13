33c96f22afe5b55ccb5d86d5371d1d6c70a67e55
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-08-14 01:37:53 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ const refs = {
   btnStart: document.querySelector('button[data-start]'),
   btnStop: document.querySelector('button[data-stop]'),
 };
-
+refs.btnStop.setAttribute('disabled', true);
 let timerId = null;
 refs.btnStart.addEventListener('click', handleOnStartBtn);
 refs.btnStop.addEventListener('click', handleOnStop);
@@ -15,8 +15,10 @@ function handleOnStartBtn() {
     document.body.style.background = getRandomHexColor();
   }, 1000);
   refs.btnStart.setAttribute('disabled', true);
+  refs.btnStop.removeAttribute('disabled', true);
 }
 function handleOnStop() {
   refs.btnStart.removeAttribute('disabled', true);
+  refs.btnStop.setAttribute('disabled', true);
   clearInterval(timerId);
 }
